0ba0a3342504e51ac4fa1cfe618f0332a9edcd3e
ASUS <aoukshdhoke@gmail.com>
2026-08-15 10:41:02 +0530

    I have Created Registration Frontend and login Frontend

--- app/src/main/java/com/example/ctrlandroid/SplashActivity.java
+++ app/src/main/java/com/example/ctrlandroid/SplashActivity.java
@@ -1,12 +1,14 @@
 package com.example.ctrlandroid;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class splash_activity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +19,7 @@ public class splash_activity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(splash_activity.this, LoginActivity.class);
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(i);
                 finish();
             }
