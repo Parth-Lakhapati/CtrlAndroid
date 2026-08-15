815eeca3f35099daf69d04a403a1abad26ed649f
ommankar3697-max <omankar3697@gmail.com>
2026-08-15 15:59:51 +0530

    api are connected to login and regiatration

--- app/src/main/java/com/example/ctrlandroid/HomeActivity.java
+++ app/src/main/java/com/example/ctrlandroid/HomeActivity.java
@@ -1,26 +1,89 @@
 package com.example.ctrlandroid;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.ctrlandroid.Fragment.HomeFragment;
+import com.example.ctrlandroid.Fragment.NewFragment;
+import com.example.ctrlandroid.Fragment.ProfileFragment;
+import com.example.ctrlandroid.Fragment.RegistrationFragment;
+import com.example.ctrlandroid.Fragment.SearchFragment;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    FrameLayout btmMenuHomeFrame;
+    BottomNavigationView btmMenuHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        btmMenuHomeFrame=findViewById(R.id.BottomMenuHomeFrame);
+        btmMenuHome=findViewById(R.id.HomeBottomMenuHome);
+        btmMenuHome.setOnNavigationItemSelectedListener(this);
+        btmMenuHome.setSelectedItemId(R.id.BottomMenuHomeFrame);
+
+        boolean isFirstTime=preferences.getBoolean("isFirstTime",true);
+        if(isFirstTime){
+            welcome();
+        }
+    }
+
+    private void welcome() {
+        AlertDialog.Builder ad=new AlertDialog.Builder(HomeActivity.this);
+        ad.setTitle("WELCOME....");
+        ad.setMessage("Thank you for login in our app DSY College Finder");
+        ad.setPositiveButton("Thank you..", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        }).show().create();
+        editor.putBoolean("isFirstTime",false).commit();
+    }
+
+    HomeFragment homeFragment = new HomeFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    NewFragment newFragment = new NewFragment();
+    RegistrationFragment registrationFragment = new RegistrationFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        if(menuItem.getItemId()==R.id.itemHomeBottomHome){
+            getSupportFragmentManager().beginTransaction().replace(R.id.BottomMenuHomeFrame,homeFragment).commit();
+        }else if(menuItem.getItemId()==R.id.itemHomeBottomSearch){
+            getSupportFragmentManager().beginTransaction().replace(R.id.BottomMenuHomeFrame,searchFragment).commit();
+        } else if (menuItem.getItemId()==R.id.itemHomeBottomNew) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.BottomMenuHomeFrame,newFragment).commit();
+        } else if (menuItem.getItemId()==R.id.itemHomeBottomRegistration){
+            getSupportFragmentManager().beginTransaction().replace(R.id.BottomMenuHomeFrame,registrationFragment).commit();
+        } else if (menuItem.getItemId()==R.id.itemHomeBottomProfile) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.BottomMenuHomeFrame,profileFragment).commit();
+        }
+
+        return true;
     }
 }
