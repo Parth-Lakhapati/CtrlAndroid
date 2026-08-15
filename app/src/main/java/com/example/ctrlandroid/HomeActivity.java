7646eea255cc51a5877cc92197cf08b9cf332fd6
ommankar3697-max <omankar3697@gmail.com>
2026-08-15 14:45:54 +0530

    urls , login database , registratio data base and some others backend is done

--- app/src/main/java/com/example/ctrlandroid/HomeActivity.java
+++ app/src/main/java/com/example/ctrlandroid/HomeActivity.java
@@ -1,12 +1,19 @@
 package com.example.ctrlandroid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class HomeActivity extends AppCompatActivity {
 
